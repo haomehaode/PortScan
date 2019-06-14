be39029bf8a6c61bfc4e84c3a38d0509fd92cc5d
超众 杨 <742610722@qq.com>
2019-06-14 15:15:08 +0800

    first commit

---
+++ 网络协议分析/端口扫描源程序/端口扫描/Form1.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace 端口扫描
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblNow = new System.Windows.Forms.Label();
+            this.lblStop = new System.Windows.Forms.Label();
+            this.lblStart = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.btnScan = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtEnd = new System.Windows.Forms.TextBox();
+            this.txtStart = new System.Windows.Forms.TextBox();
+            this.lbResult = new System.Windows.Forms.ListBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.ipAddressTextBox1 = new IPText.IPAddressTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.ipAddressTextBox1);
+            this.splitContainer1.Panel1.Controls.Add(this.button1);
+            this.splitContainer1.Panel1.Controls.Add(this.lblNow);
+            this.splitContainer1.Panel1.Controls.Add(this.lblStop);
+            this.splitContainer1.Panel1.Controls.Add(this.lblStart);
+            this.splitContainer1.Panel1.Controls.Add(this.progressBar1);
+            this.splitContainer1.Panel1.Controls.Add(this.btnScan);
+            this.splitContainer1.Panel1.Controls.Add(this.label3);
+            this.splitContainer1.Panel1.Controls.Add(this.label2);
+            this.splitContainer1.Panel1.Controls.Add(this.label1);
+            this.splitContainer1.Panel1.Controls.Add(this.txtEnd);
+            this.splitContainer1.Panel1.Controls.Add(this.txtStart);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.lbResult);
+            this.splitContainer1.Size = new System.Drawing.Size(581, 299);
+            this.splitContainer1.SplitterDistance = 243;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(134, 214);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "停止";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblNow
+            // 
+            this.lblNow.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNow.Location = new System.Drawing.Point(102, 151);
+            this.lblNow.Name = "lblNow";
+            this.lblNow.Size = new System.Drawing.Size(48, 23);
+            this.lblNow.TabIndex = 23;
+            // 
+            // lblStop
+            // 
+            this.lblStop.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStop.Location = new System.Drawing.Point(178, 151);
+            this.lblStop.Name = "lblStop";
+            this.lblStop.Size = new System.Drawing.Size(48, 23);
+            this.lblStop.TabIndex = 22;
+            // 
+            // lblStart
+            // 
+            this.lblStart.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStart.Location = new System.Drawing.Point(26, 151);
+            this.lblStart.Name = "lblStart";
+            this.lblStart.Size = new System.Drawing.Size(48, 23);
+            this.lblStart.TabIndex = 21;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(26, 119);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(200, 23);
+            this.progressBar1.Step = 1;
+            this.progressBar1.TabIndex = 20;
+            // 
+            // btnScan
+            // 
+            this.btnScan.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnScan.Location = new System.Drawing.Point(29, 214);
+            this.btnScan.Name = "btnScan";
+            this.btnScan.Size = new System.Drawing.Size(75, 23);
+            this.btnScan.TabIndex = 19;
+            this.btnScan.Text = "扫描";
+            this.btnScan.Click += new System.EventHandler(this.btnScan_Click_1);
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(26, 55);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 23);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "起始端口";
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(26, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 23);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "结束端口";
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(26, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 23);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "主机地址";
+            // 
+            // txtEnd
+            // 
+            this.txtEnd.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEnd.Location = new System.Drawing.Point(90, 87);
+            this.txtEnd.Name = "txtEnd";
+            this.txtEnd.Size = new System.Drawing.Size(125, 22);
+            this.txtEnd.TabIndex = 15;
+            // 
+            // txtStart
+            // 
+            this.txtStart.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtStart.Location = new System.Drawing.Point(90, 55);
+            this.txtStart.Name = "txtStart";
+            this.txtStart.Size = new System.Drawing.Size(125, 22);
+            this.txtStart.TabIndex = 14;
+            // 
+            // lbResult
+            // 
+            this.lbResult.Dock = System.Windows.Forms.DockStyle.Right;
+            this.lbResult.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbResult.ItemHeight = 14;
+            this.lbResult.Location = new System.Drawing.Point(-1, 0);
+            this.lbResult.Name = "lbResult";
+            this.lbResult.Size = new System.Drawing.Size(335, 299);
+            this.lbResult.TabIndex = 1;
+            // 
+            // ipAddressTextBox1
+            // 
+            this.ipAddressTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.ipAddressTextBox1.Location = new System.Drawing.Point(90, 21);
+            this.ipAddressTextBox1.Name = "ipAddressTextBox1";
+            this.ipAddressTextBox1.Size = new System.Drawing.Size(125, 28);
+            this.ipAddressTextBox1.TabIndex = 24;
+            this.ipAddressTextBox1.Value = ((System.Net.IPAddress)(resources.GetObject("ipAddressTextBox1.Value")));
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(581, 299);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "端口扫描";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Label lblNow;
+        private System.Windows.Forms.Label lblStop;
+        private System.Windows.Forms.Label lblStart;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button btnScan;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtEnd;
+        private System.Windows.Forms.TextBox txtStart;
+        private System.Windows.Forms.ListBox lbResult;
+        private System.Windows.Forms.Button button1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private IPText.IPAddressTextBox ipAddressTextBox1;
+
+    }
+}
+
---
+++ 网络协议分析/端口扫描源程序/端口扫描/Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 端口扫描
+{
+    public partial class Form1 : Form
+    {
+        /// <summary>
+        /// 端口号
+        /// </summary>
+        private int port;
+        /// <summary>
+        /// IP地址
+        /// </summary>
+        private string IP;
+        /// <summary>
+        /// 数组
+        /// </summary>
+        private bool[] done = new bool[65536];
+        /// <summary>
+        /// 开始端口号
+        /// </summary>
+        private int start;
+        /// <summary>
+        /// 结束端口号
+        /// </summary>
+        private int end;
+        /// <summary>
+        /// 线程
+        /// </summary>
+        private Thread scanThread;
+        /// <summary>
+        /// 是否完成
+        /// </summary>
+        private bool isOK;
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        public Form1()
+        {
+            InitializeComponent();
+            CheckForIllegalCrossThreadCalls = false;
+        }
+        /// <summary>
+        /// 扫描线程
+        /// </summary>
+        private void PortScan()
+        {
+            start = Int32.Parse(txtStart.Text);
+            end = Int32.Parse(txtEnd.Text);
+            //检查输入范围合法性
+            if ((start >= 0 && start <= 65536) && (end >= 0 && end <= 65536) && (start <= end))
+            {
+                lbResult.Items.Add("开始扫描... (可能需要请您等待几分钟)");
+
+                IP = ipAddressTextBox1.Text;
+                for (int i = start; i <= end; i++)
+                {
+                    port = i;
+                    //使用该端口的扫描线程
+                    scanThread = new Thread(new ThreadStart(Scan));
+                    scanThread.Start();
+                    //使线程睡眠
+                    System.Threading.Thread.Sleep(100);
+                    progressBar1.Value = i;
+                    lblNow.Text = i.ToString();
+                }
+                //未完成时情况
+                while (!isOK)
+                {
+                    isOK = true;
+                    for (int i = start; i <= end; i++)
+                    {
+                        if (!done[i])
+                        {
+                            isOK = false;
+                            break;
+                        }
+                    }
+                    System.Threading.Thread.Sleep(1000);
+                }
+                lbResult.Items.Add("扫描结束!");
+            }
+            else
+            {
+                MessageBox.Show("输入错误，端口范围为[0-65536]");
+            }
+        }
+
+        private void Scan()
+        {
+            int portnow = port;
+            //创建线程变量
+            Thread Threadnow = scanThread;
+            done[portnow] = true;
+            //创建TcpClient对象，TcpClient用于为TCP网络服务提供客户端连接
+            TcpClient objTCP = null;
+            //扫描端口，成功则写入信息
+            try
+            {
+                //用TcpClient对象扫描端口
+                objTCP = new TcpClient(IP, portnow);
+                lbResult.Items.Add("端口 " + portnow.ToString() + " 开放!");
+            }
+            catch
+            {
+                //lbResult.Items.Add("端口 " + portnow.ToString() + " 未开放!");
+            }
+        }
+        public Thread process;
+        private void btnScan_Click_1(object sender, EventArgs e)
+        {
+            btnScan.Enabled = false;
+            if (txtStart.Text.ToString() == "")
+            {
+                MessageBox.Show("IP地址不能为空");
+                return;
+            }
+          
+            //创建线程，并创建ThreadStart委托对象
+            process = new Thread(new ThreadStart(PortScan));
+            //线程开始
+            process.Start();
+            //显示端口扫描的范围
+            progressBar1.Minimum = Int32.Parse(txtStart.Text);
+            progressBar1.Maximum = Int32.Parse(txtEnd.Text);
+            //显示框初始化
+            lbResult.Items.Clear();
+            lbResult.Items.Add("            端口扫描器           ");
+            lbResult.Items.Add("---------------------------------"); 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            process.Abort();
+            btnScan.Enabled = true;
+           // progressBar1.Value = 0;
+             
+        }
+    }
+}
